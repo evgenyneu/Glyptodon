f05592179f59e874f7c7e39cc4a2e97da5a9e8bb
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-10-08 12:58:19 +1100

    Initial Commit

---
+++ Glyptodon/Glyptodon.h
@@ -0,0 +1,19 @@
+//
+//  Glyptodon.h
+//  Glyptodon
+//
+//  Created by Evgenii Neumerzhitckii on 8/10/2015.
+//  Copyright © 2015 Marketplacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Glyptodon.
+FOUNDATION_EXPORT double GlyptodonVersionNumber;
+
+//! Project version string for Glyptodon.
+FOUNDATION_EXPORT const unsigned char GlyptodonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glyptodon/PublicHeader.h>
+
+
