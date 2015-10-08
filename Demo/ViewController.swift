c6cf1b888a3f1919b89581890647b0c25fbe19e0
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-10-08 14:21:52 +1100

    Add demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -1,11 +1,14 @@
 import UIKit
+import Glyptodon
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
   }
 
+  @IBAction func didTapShowButton(sender: AnyObject) {
+    view.glyptodon.show("No new messages.")
+  }
 }
 
--- Glyptodon/GlyptodonView.swift
+++ Glyptodon/GlyptodonView.swift
@@ -3,10 +3,38 @@ import UIKit
 class GlyptodonView: UIView {
   func showInSuperview(superview: UIView, withMessage message: String) {
     superview.addSubview(self)
-    
+    addLayoutConstraints()
+    createLabel(message)
+    style()
+  }
+  
+  private func createLabel(message: String) {
     let label = UILabel()
     label.text = message
+    label.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+    label.textColor = UIColor.redColor()
+    label.textAlignment = .Center
     
     addSubview(label)
+    addLabelLayoutConstraints(label)
+  }
+  
+  private func style() {
+    backgroundColor = UIColor.grayColor()
+  }
+  
+  private func addLayoutConstraints() {
+    guard let superview = superview else { return }
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    TegAutolayoutConstraints.fillParent(self, parentView: superview, margin: 0, vertically: true)
+    TegAutolayoutConstraints.fillParent(self, parentView: superview, margin: 0, vertically: false)
+  }
+  
+  private func addLabelLayoutConstraints(label: UILabel) {
+    label.translatesAutoresizingMaskIntoConstraints = false
+    TegAutolayoutConstraints.fillParent(label, parentView: self, margin: 20, vertically: false)
+    TegAutolayoutConstraints.centerY(label, viewTwo: self, constraintContainer: self)
   }
 }
